b6d37af83bf4580d4f9fb9c91fad9db92a7cab43
Selin Kubilay <kubilay@igb-berlin.de>
2023-03-11 10:19:08 -0800

    plotting

--- R/Column_DOC_Calculations.R
+++ R/Column_DOC_Calculations.R
@@ -3,175 +3,162 @@ library(ggplot2)
 library(data.table)
 
 ####Import the data####
-DOC_data =data.table(read.csv("data/DOC_Final_pretreated_all.csv", col.names = c("Sample", "Date", "DOC", "DN")))
-DOC_data$Date=as.Date(DOC_data$Date, "%m/%d/%Y")
-
-# Define which data is the reservoir standard which are the samples
-reservoir = data.table(DOC_data[DOC_data$Sample %like% c("Reservoir")])
-samples = subset(DOC_data, !(DOC_data$Sample %like% c("Reservoir")))
+DOC_data = data.table(read.csv("data/DOC_Final_pretreated_all.csv", col.names = c("Sample", "Date", "DOC", "DN")))
+DOC_data$Date = as.Date(DOC_data$Date, "%m/%d/%Y")
 
 #Get the sample name as it's sampling occasion (data), replicate name and column order
-samples[,c("sample_date", "replicate", "column_no") := tstrsplit(Sample, "_")]
-list(samples,  samples$sample_date) #Check the sample names for errors
+DOC_data[,c("sample_date", "replicate", "column_no") := tstrsplit(Sample, "_")]
+list(DOC_data,  DOC_data$sample_date) #Check the sample names for errors
 
 #Same for the whole data, separate the name code to date, replicate name and column number
 DOC_data[,c("sample_date", "replicate", "column_no") := tstrsplit(Sample, "_")]
-DOC_data[.=lapply(.SD, as.factor), by=c("Sample", "Date", "DOC", "DN")] #Convvert to factor and set the levels, this is important bcs the names contadict the alphabetical order but importnant to level correclty for plotting
-DOC_data$column_no=factor(DOC_data$column_no, levels=c("C0", "C1", "C2", "C3"))
+DOC_data[.= lapply(.SD, as.factor), by = c("Sample", "Date", "DOC", "DN")] #Convvert to factor and set the levels, this is important bcs the names contadict the alphabetical order but importnant to level correclty for plotting
+DOC_data$column_no = factor(DOC_data$column_no, levels = c("C0", "C1", "C2", "C3"))
 
-DOC_data=DOC_data[DOC<5000] # These are outliers (contamination in this case when DOC>5mgC/L)
-DOC_data=subset(DOC_data, subset=!(sample_date>"S10" & replicate=="O")) # These are an experimental error. So the replicate O was flooded at this date
+DOC_data = DOC_data[DOC < 5000] # These are outliers (contamination in this case when DOC>5mgC/L)
+DOC_data = subset(DOC_data, subset =! (sample_date > "S10" & replicate == "O")) # These are an experimental error. So the replicate O was flooded at this date
+write.csv(DOC_data, "DOC_per_column.csv")
+
+C0 = DOC_data[column_no == "C0"]
+colnames(C0) = c("Sample", "Date","DOC_C0","DN_C0","sample_date", "replicate","column_no")
+C1 = DOC_data[column_no == "C1"]
+colnames(C1) = c("Sample", "Date","DOC_C1","DN_C1","sample_date", "replicate","column_no")
+C2 = DOC_data[column_no == "C2"]
+colnames(C2) = c("Sample", "Date","DOC_C2","DN_C2","sample_date", "replicate","column_no")
+C3 = DOC_data[column_no == "C3"]
+colnames(C3) = c("Sample", "Date","DOC_C3","DN_C3","sample_date", "replicate","column_no")
 
 
-C0=DOC_data[column_no=="C0"]
-C1=DOC_data[column_no=="C1"]
-C2=DOC_data[column_no=="C2"]
-C3=DOC_data[column_no=="C3"]
+DOC_consumption = C0[C1, on = .(sample_date = sample_date, replicate = replicate)][C2, on = .(sample_date = sample_date, replicate = replicate)][C3, on = .(sample_date = sample_date, replicate = replicate)]
+DOC_consumption[, `:=`(C1consumed = DOC_C0-DOC_C1, C2consumed = DOC_C1-DOC_C2, C3consumed = DOC_C2-DOC_C3)]
 
-C0$C1consumed=C0$DOC - C1$DOC[match(C0$replicate, C1$replicate)]
-C1$C2consumed=C1$DOC - C2$DOC[match(C1$replicate, C2$replicate)]
-C2$C3consumed=C2$DOC - C3$DOC[match(C2$replicate, C3$replicate)]
-
-data=C0[C1[,c("sample_date", "replicate", "C2consumed")], on=.(sample_date, replicate)]
-data=data[C2[,c("sample_date", "replicate", "C3consumed")], on=.(sample_date, replicate)]
-data=data[,c("replicate", "sample_date", "C1consumed","C2consumed","C3consumed")]
-
-write.csv(data, "DOC_consumption.csv")
-
+write.csv(DOC_consumption, "DOC_consumption.csv")
 
 #Put the individual sample names to be deleted. These sampling dates follows the contamination days, so no need to have them either. 
 #It is important to have their names individually
-DOC_data=DOC_data[!DOC_data$Sample %in% c("S07_A_C0","S08_A_C1", "S08_A_C2","S08_A_C3,",
+DOC_data = DOC_data[!DOC_data$Sample %in% c("S07_A_C0","S08_A_C1", "S08_A_C2","S08_A_C3,",
                                   "S08_G_C0", "S08_G_C1","S08_G_C2","S08_G_C3,")]
 
 #Calculate the means of the same day replications
-mean_values=DOC_data[ ,.(Mean=mean(DOC, na.rm=T)), by=.(sample_date, column_no)]
+mean_values = DOC_data[ ,.(Mean = mean(DOC, na.rm = TRUE)), by = .(sample_date, column_no)]
 
 #The general average plot with S05 ans S06 discareded since these are still data that needs justiciation to set as outlier
 source("R/plotting_functions.R")
-ggplot(DOC_data[!(sample_date=="S05"| sample_date=="S06")])+
- facet_grid( ~sample_date)+
- geom_line(aes(y=DOC, x=column_no, group=replicate, color=replicate))+
- geom_line(data=mean_values[!(sample_date=="S05"| sample_date=="S06")], aes(x=column_no, y=Mean, group=sample_date), color="red", lwd=2)+
- geom_point(data=mean_values[!(sample_date=="S05"| sample_date=="S06")], aes(x=column_no, y=Mean, group=sample_date), color="blue", lwd=5)+
- theme(legend.position="right")+theme_bw()
+ggplot(DOC_data[!(sample_date == "S05"| sample_date == "S06")]) +
+ facet_grid( ~sample_date) +
+ geom_line(aes(y = DOC, x = column_no, group = replicate, color = replicate)) +
+ geom_line(data = mean_values[!(sample_date == "S05"| sample_date == "S06")], aes(x = column_no, y = Mean, group = sample_date), color = "red", lwd = 2) +
+ geom_point(data = mean_values[!(sample_date  == "S05"| sample_date == "S06")], aes(x = column_no, y = Mean, group = sample_date), color = "blue", lwd = 5) +
+ theme(legend.position = "right") + theme_bw()
 
-ggplot(DOC_data[replicate=="F"])+
-  facet_grid( ~sample_date)+
-  geom_line(aes(y=DOC, x=column_no, group=replicate), color="yellow1", lwd=2)+
-  theme(legend.position="right")+theme_bw()
+ggplot(DOC_data[replicate == "F"]) +
+  facet_grid( ~sample_date) +
+  geom_line(aes(y = DOC, x = column_no, group = replicate), color = "yellow1", lwd = 2) +
+  theme(legend.position = "right") + theme_bw()
 
 #Calculate the carbon consumption of each column
-table = dcast(data=mean_values, sample_date ~ column_no)
-table=table[,.(col1=C0-C1, col2=C1-C2, col3=C2-C3), by=.(sample_date)]
-table=melt(table)
-#Check for C increasing columns
-ggplot(table)+
-  facet_grid(~sample_date)+
-  geom_col(aes(x=variable, y=value))
+table = dcast(data = mean_values, sample_date ~ column_no)
+table = table[,.(col1 = C0-C1, col2 = C1-C2, col3 = C2-C3), by = .(sample_date)]
+table = melt(table)
 
 #For different palettes for the facets, do interaction(variable, sample_date>"S10") but I do it with hue for now
 ggplot(table)+
-  facet_grid(~sample_date>"S10", scales="free_x", labeller=as_labeller(c('FALSE'="Before Treatment", 'TRUE'="After Treatment")))+
-  scale_fill_manual(values=scico::scico(palette="bamO", 6, alpha=1, direction=-1, begin=0.1, end=0.7), 
-                    labels=c('Col 1', 'Col 2', 'Col 3', 'Col 1', 'Col 2', 'Col 3'), name=NULL)+
-  geom_col(aes(x=sample_date, y=value, fill=interaction(variable, sample_date>"S10")))+theme_bw()+theme(panel.grid = element_blank())+
+  facet_grid(~sample_date>"S10", scales = "free_x", labeller = as_labeller(c('FALSE' = "Before Treatment", 'TRUE' = "After Treatment")))+
+  scale_fill_manual(values = scico::scico(palette = "bamO", 6, alpha = 1, direction = -1, begin = 0.1, end = 0.7), 
+                    labels = c('Col 1', 'Col 2', 'Col 3', 'Col 1', 'Col 2', 'Col 3'), name = NULL)+
+  geom_col(aes(x = sample_date, y = value, fill = interaction(variable, sample_date>"S10")))+theme_bw()+theme(panel.grid = element_blank())+
   xlab("Sampling day")+ylab(expression(paste(Delta, "DOC (um C/L)")))
 
-data2=table[!table$sample_date%in%c("S05", "S06")]
+data2 = table[!table$sample_date %in% c("S05", "S06")]
 
+convert_date_labels(data2)
 ggplot(data2)+
-  facet_grid(~sample_date>"S10", scales="free_x", labeller=as_labeller(c('FALSE'="Before Treatment", 'TRUE'="After Treatment")))+
-  scale_fill_manual(values=scico::scico(palette="bamO", 6, alpha=1, direction=-1, begin=0.1, end=0.7), 
-                    labels=c('Col 1', 'Col 2', 'Col 3', 'Col 1', 'Col 2', 'Col 3'), name=NULL)+
-  geom_col(aes(x=sample_date, y=value, fill=interaction(variable, sample_date>"S10")))+theme_bw()+theme(panel.grid = element_blank())+
+  facet_grid(~sample_date>"S10", scales = "free_x", labeller = as_labeller(c('FALSE' = "Before Treatment", 'TRUE' = "After Treatment")))+
+  scale_fill_manual(values = scico::scico(palette = "bamO", 6, alpha = 1, direction = -1, begin = 0.1, end = 0.7), 
+                    labels = c('Col 1', 'Col 2', 'Col 3', 'Col 1', 'Col 2', 'Col 3'), name = NULL)+
+  geom_col(aes(x = sample_date, y = value, fill = interaction(variable, sample_date>"S10")))+theme_bw()+theme(panel.grid  =  element_blank())+
   xlab("Sampling day")+ylab(expression(paste(Delta, "DOC (um C/L)")))
 
-### The proportional C consumption plots
-ggplot(data2)+
-  facet_grid(~sample_date>"S10", scales="free_x", labeller=as_labeller(c('FALSE'="Before Reversal", 'TRUE'="After Reversal")))+
-  scale_fill_manual(values=scico::scico(palette="bamO", 6, alpha=1, direction=-1, begin=0.1, end=0.7), 
-                    labels=c('Col 1', 'Col 2', 'Col 3', 'Col 1', 'Col 2', 'Col 3'), name=NULL)+
-  geom_col(position="fill", aes(x=sample_date, y=value, fill=interaction(variable, sample_date>"S10")))+
-  theme_bw()+theme(panel.grid = element_blank())
 
 ### Other graphs
 # DOC consumption in each column throughout the sampling time
-ggplot(data2)+
-  facet_wrap(~variable)+
-  geom_point(aes(x=sample_date, y=value))+
-  geom_vline(xintercept="S10", color="red", linetype="dashed")
+ggplot(data2) +
+  facet_wrap(~variable) +
+  geom_point(aes(x = sample_date, y = value)) +
+  geom_vline(xintercept = "S10", color = "red", linetype = "dashed")
 
 ### Biomass correction of c consumtion
-Sample_biomass=fread("C:/Users/c7701233/Nextcloud/Column-Experiment/EEA/BActerial_Abundance/Cell_Counts_standardized.csv")
-Sample_biomass$col_no=factor(Sample_biomass$col_no, levels = c("1", "2", "3"), labels=c("Col1", "Col2", "Col3"))
+Sample_biomass = fread("C:/Users/c7701233/Nextcloud/Column-Experiment/EEA/BActerial_Abundance/Cell_Counts_standardized.csv")
+Sample_biomass$col_no = factor(Sample_biomass$col_no, levels = c("1", "2", "3"), labels = c("Col1", "Col2", "Col3"))
 
-data_subset=data[sample_date%in%c("S10","S11","S12" ,"S13", "S14","S15","S16", "S17","S18","S19")]
-data_subset[sample_date=="S10"]$sample_date="S09"
-colnames(data_subset)=c("replicate", "sample_date", "Col1", "Col2", "Col3")
-data_subset2=melt(data_subset, id.vars = c("replicate", "sample_date"), 
-     measure.vars=c("Col1","Col2","Col3"))
-data_subset2$sample_date=factor(data_subset2$sample_date, levels = c("S09","S11","S12" ,"S13", "S14","S15","S16", "S17","S18","S19"), 
-                                                          labels=c("Before \nReversal", "Day1","Day2","Day3", "Day7","Day9", "Day10","Day12","Day14","Day17"))
+data_subset = DOC_consumption[sample_date %in% c("S10","S11","S12" ,"S13", "S14","S15","S16", "S17","S18","S19")]
+data_subset[sample_date == "S10"]$sample_date = "S09"
+data_subset2 = melt(data_subset, id.vars = c("replicate", "sample_date"), 
+     measure.vars = c("C1consumed","C2consumed","C3consumed"))
 
-date_labels = c("Day1","Day2","Day3", "Day7","Day9", "Day10","Day12","Day14","Day17")
-data_subset2$highlight = factor(ifelse(data_subset2$sample_date=="Before \nReversal" & data_subset2$variable=="Col1", "before C1", 
-                                ifelse(data_subset2$sample_date=="Before \nReversal" & data_subset2$variable=="Col2", "before C2",
-                                ifelse(data_subset2$sample_date=="Before \nReversal" & data_subset2$variable=="Col3", "before C3", 
-                                ifelse(data_subset2$sample_date %in% date_labels & data_subset2$variable=="Col1", "after C1",
-                                       ifelse(data_subset2$sample_date %in% date_labels & data_subset2$variable=="Col2", "after C2",
-                                              ifelse(data_subset2$sample_date %in% date_labels & data_subset2$variable=="Col3", "after C3",
-                                "NA")))))))
+data_subset2$variable = factor(data_subset2$variable, levels = c("C1consumed", "C2consumed", "C3consumed"), 
+                             labels = c("Col1", "Col2", "Col3"))
 
+data_all = merge(data_subset2, Sample_biomass, all.x = FALSE, 
+               by.x = c("replicate", "sample_date", "variable"), 
+               by.y = c("replicate", "Sample_date", "col_no"))
 
+data_subset2 = set_coloring_column(data_subset2)
+data_subset2 = convert_date_labels(data_subset2)
+reservoir = unique(data_subset[, .(DOC_C0, sample_date)])
 
-data_all=merge(data_subset2, Sample_biomass, all.x=F, 
-      by.x=c("replicate", "sample_date", "variable"), 
-      by.y=c("replicate", "Sample_date", "col_no"))
-data_all$sample_date=factor(data_all$sample_date, levels = c("S09", "S11", "S13", "S16", "S19"), labels=c("Before \nReversal", "Day1","Day3", "Day10", "Day17"))
+DOC = ggplot(data_subset2, aes(x = sample_date, y = (value))) +
+  facet_grid(~variable) +
+  geom_boxplot(aes(fill = highlight, color = highlight), width=0.5) +
+  observation_numbers() +
+  optical_plots_theme() + 
+  fill_col_no() + color_col_no() +
+  ylab("ΔDOC(Δ ug C/L)") + xlab ("Days")
 
+data_all[, log_cell_pro_ml := (log(Cell_pro_ml))]
+data_all[, plotting_cell_pro_ml := (Cell_pro_ml)/10^8]
+data_all[, log_normalized_doc := (value / log_cell_pro_ml)]
+data_all[, plotting_normalized_doc := (value / plotting_cell_pro_ml)]
+data_all = set_coloring_column(data_all)
+data_all = convert_date_labels(data_all)
+data_all = data_all[-45, ]#quick remove the replicated sample
 
-data_all[,normalized_doc:=(value/Cell_pro_ml)]
-
-ggplot(data_subset2)+
+NDOC = ggplot(data_all, aes(y = (plotting_normalized_doc), x = sample_date))+
   facet_grid( ~variable)+
-  geom_boxplot(aes(y=(value), x=sample_date, fill=highlight))+
-  theme_boxplot()+fill_column_color()+
-  ylab("ΔDOC(Δ ug C/L)")
+  geom_boxplot(aes(fill = highlight, color = highlight), width = 0.5)+
+  observation_numbers()+
+  optical_plots_theme()+ fill_col_no() + color_col_no()+
+  ylab(bquote(Δ~um~C~L^-1*~(10^8~cell)^-1))
 
-ggplot(data_subset2)+
-  facet_grid(~variable)+
-  geom_boxplot(aes(y=(value), x=sample_date, fill=highlight))+
-  theme_boxplot()+fill_sample_date_col_no()+
-  ylab("ΔDOC(Δ ug C/L)")
+Bio = ggplot(data_all, aes(y = (Cell_pro_ml / 10^8), x = sample_date)) +
+  facet_grid( ~variable) +
+  geom_boxplot(aes(fill = highlight, color = highlight), width = 0.5) + 
+  observation_numbers() +
+  optical_plots_theme() + fill_col_no() + color_col_no() +
+  ylab(bquote("Biomass "(10^8*cell~ml^-1))) + xlab ("Days")
 
-ggplot(data_subset2)+
-  facet_grid(rows=vars(variable))+
-  geom_boxplot(aes(y=(value), x=sample_date, fill=highlight))+
-  theme_boxplot()+fill_sample_date_col_no()+
-  ylab("ΔDOC(Δ ug C/L)")
+#Facet plot for all
+require(gridExtra)
 
-ggplot(data_subset2)+
+grid.arrange(DOC, Bio, NDOC, ncol=1)
+
+## Dissolved Nitrogen plot
+DOC_data = DOC_data[sample_date%in%c("S08", "S10","S11","S12" ,"S13", "S14","S15","S16", "S17","S18","S19")]
+DOC_data[sample_date == "S10"]$sample_date = "S09"
+DOC_data$column_no = factor(DOC_data$column_no, levels = c("C0", "C1", "C2", "C3"), labels = c("Reservoir","Col1", "Col2", "Col3"))
+colnames(DOC_data) = c("Sample", "Date", "DOC", "DN", "sample_date", "replicate", "variable")
+
+DOC_data = set_coloring_column(DOC_data)
+DOC_data = convert_date_labels(DOC_data)
+
+ggplot(DOC_data, aes(y = (DN), x = sample_date))+
   facet_grid( ~variable)+
-  geom_boxplot(aes(y=(value), x=sample_date, fill=highlight))+
-  theme_boxplot()+fill_sample_date_col_no()+
-  ylab("ΔDOC(Δ ug C/L)")+coord_flip()
-
-ggplot(data_all)+
-  facet_grid( ~variable)+
-  geom_boxplot(aes(y=(normalized_doc), x=sample_date, fill=sample_date))+
-  theme_boxplot()+fill_selected()+
-  ylab("Biomass Normalized ΔDOC \n (Δ um C/L*Bacterium?)")
-
-ggplot(data_all)+
-  facet_grid( ~variable)+
-  geom_boxplot(aes(y=(Cell_pro_ml), x=sample_date, fill=sample_date))+
-  theme_boxplot()+fill_selected()+
-  ylab("Biomass (cell/ml sample)")
-
-
+  geom_boxplot(aes(fill = highlight, color = highlight))+ 
+ #observation_numbers()+
+  optical_plots_theme()+ fill_col_no() + color_col_no()+
+  ylab("DN (mg/L)")
 
 #Anova on DOC consumption
-anova=aov((data_all$normalized_doc)~(sample_date*variable), data=data_all)
+anova = aov((data_all$normalized_doc)~(sample_date*variable), data = data_all)
 summary(anova)
 TukeyHSD(anova)
--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -1,50 +1,52 @@
 
 optical_plots_theme = function(){
   theme_bw()+
-    theme(axis.text = element_text(size=10), 
-          axis.title = element_text(size=12), 
-          text =  element_text(size=10),
-          axis.text.x = element_text(size=10, angle=45, vjust=1, hjust=1))
+    theme(axis.text = element_text(size = 10), 
+          axis.title = element_text(size = 12), 
+          text =  element_text(size = 10),
+          axis.text.x = element_text(size = 10, vjust = 1, hjust = 0.5))
 }
 
-set_coloring_column = function(data) {
-  data$highlight = factor(ifelse(data$sample_date=="S09" & data$variable=="Col1", "before C1", 
-                                 ifelse(data$sample_date=="S09" & data$variable=="Col2", "before C2",
-                                        ifelse(data$sample_date=="S09" & data$variable=="Col3", "before C3", 
+set_coloring_column = function(data, col_name) {
+  data$highlight = factor(ifelse(data$sample_date %in% c("S08", "S09") & data$variable == "Col1", "before C1", 
+                                 ifelse(data$sample_date %in% c("S08", "S09") & data$variable == "Col2", "before C2",
+                                        ifelse(data$sample_date %in% c("S08", "S09") & data$variable == "Col3", "before C3", 
                                                
-                                               ifelse(data$sample_date != c("S09") & data$variable=="Col1", "after C1",
-                                                           ifelse(data$sample_date != c("S09") & data$variable=="Col2", "after C2",
-                                                                      ifelse(data$sample_date != c("S09") & data$variable=="Col3", "after C3",
-                                                                                         "Reservoirs")))))))
+                                               ifelse(!data$sample_date  %in% c("S08", "S09") & data$variable == "Col1", "after C1",
+                                                           ifelse(!data$sample_date %in% c("S08", "S09")  & data$variable == "Col2", "after C2",
+                                                                      ifelse(!data$sample_date  %in% c("S08", "S09")  & data$variable == "Col3", "after C3",
+                                                                                         "Reservoir")))))))
   return(data)
 }
 
 convert_date_labels = function(data) {
-  data$sample_date=factor(data$sample_date, 
-                              levels = c("S09","S11","S12" ,"S13", "S14","S15","S16", "S17","S18","S19"), 
-                              labels=c("Day0", "Day1","Day2","Day3", "Day7","Day9", "Day10","Day12","Day14","Day17"))
+  data$sample_date = factor(data$sample_date, 
+                              levels = c("S08", "S09", "S11", "S12" ,"S13", "S14","S15", "S16", "S17","S18","S19",
+                                         "S01", "S02", "S03", "S04", "S07", "S10"), 
+                              labels = c("0", "0", "1","2","3", "7","9", "10","12","14","17",
+                                       "S01", "S02", "S03", "S04", "S07", "0"))
   return(data)
 }
 
 fill_col_no = function(){
   scale_fill_manual( name =  "Column Location",
-                     #labels=c("Column 1", "Column 2", "Column 3", "Before Reversal"), 
-                     values=c("#1741a3", "#4e8fc8", "#a698cc", "white", "white", "white"),
-                     guide="legend")
+                     #labels = c("Column 1", "Column 2", "Column 3", "Before Reversal"), 
+                     values = c("#1741a3", "#4e8fc8", "#a698cc", "white", "white", "white", "grey"),
+                     guide = "legend")
 } 
 
 color_col_no = function(){
   scale_color_manual( name =  "Column Location",
-                      #labels=c("Column 1", "Column 2", "Column 3", "Before Reversal"), 
-                      values=c("black", "black", "black", "#1741a3", "#4e8fc8", "#a698cc"),
-                      guide="legend")
+                      #labels = c("Column 1", "Column 2", "Column 3", "Before Reversal"), 
+                      values = c("black", "black", "black", "#1741a3", "#4e8fc8", "#a698cc", "black"),
+                      guide = "legend")
 } 
 
 
 n_fun = function(x){
-  return(data.frame(y = max(x, na.rm=T), label = paste0(length(x))))
+  return(data.frame(y = max(x, na.rm = TRUE), label = paste0(length(x))))
 }                                                      
 
 observation_numbers = function (x) {
-  stat_summary(fun.data = n_fun, geom = "text", na.rm=T, aes(vjust=0))
+  stat_summary(fun.data = n_fun, geom = "text", na.rm = TRUE, aes(vjust = 0))
 }
